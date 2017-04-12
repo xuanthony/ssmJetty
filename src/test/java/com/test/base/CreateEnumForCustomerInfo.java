f7279285a96b1445e4ba52f87b11e2891702d0d9
xuxinbin <xuanthony@sina.com>
2017-04-12 20:07:51 +0800

    新建枚举文档

---
+++ src/test/java/com/test/base/CreateEnumForCustomerInfo.java
@@ -0,0 +1,15 @@
+package com.test.base;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author xuxinbin
+ * @version $$Id: sky-credit, v 0.1 2017/4/12 19:24 xuxinbin Exp $$
+ */
+public class CreateEnumForCustomerInfo {
+    public static void main(String[] args) {
+        String fileName = "D:\\Users\\test001.txt";
+        String str = "cert_type";
+        System.out.println(StringUtils.upperCase(str));
+    }
+}
